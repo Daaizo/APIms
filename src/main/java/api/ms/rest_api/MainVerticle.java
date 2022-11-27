942b2e5c4b1a1b6c545741e2a69e078c72e45ed4
Daaizo <danielcharlak@wp.pl>
2022-11-27 11:00:25 +0100

    feat: http post request handled, and logger added
    data from the request is transferred directly to the mongo db

--- src/main/java/api/ms/rest_api/MainVerticle.java
+++ src/main/java/api/ms/rest_api/MainVerticle.java
@@ -1,40 +1,46 @@
 package api.ms.rest_api;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class MainVerticle extends AbstractVerticle {
   //to start from main instead of console
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
   public static void main(String[] args) {
+    BasicConfigurator.configure();
     Vertx vertx1 = Vertx.vertx();
-    vertx1.deployVerticle(new MainVerticle());
+    Router router = Router.router(vertx1);
+    HttpServer server = vertx1.createHttpServer();
+    server
+      .requestHandler(router)
+      .listen(3000)
+      .onSuccess(server1 -> logger.info("server listening on port " + server1.actualPort()))
+      .onFailure(throwable -> System.out.println("throwable = " + throwable.getCause()));
+
+    //router - controller
+    router
+      .post("/add")
+      .handler(User::addUser);
+
+
+//
+//    User u1 = new User("daniel", "12");
+//    Route deafultRoute =
+//      router.get().handler(context1 -> {
+//        context1
+//          .response()
+//          .setStatusCode(201)
+//          .end(u1.getUserAsJson().toString());
+//      });
+
+
   }
 
-  private static void testRequest(HttpServerRequest request) {
-    request
-      .response()
-      .putHeader("content-type", "text/plain")
-      .end("Server up");
-  }
-
-  @Override
-  public void start(Promise<Void> startPromise) {
-    HttpServer httpServer = vertx.createHttpServer();
-    httpServer
-      .requestHandler(MainVerticle::testRequest)
-      .listen(3000,httpServerAsyncResult -> {
-          if(httpServerAsyncResult.succeeded()){
-            System.out.println("HTTP server started on port " + httpServer.actualPort());
-            startPromise.complete();
-          }else{
-            startPromise.fail(httpServerAsyncResult.cause());
-          }
-      });
-
-
-
-  }
 }
