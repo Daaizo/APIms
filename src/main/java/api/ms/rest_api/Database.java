5716b34204fcf0e8ab976cab3fc7c74183b09d4b
Daaizo <danielcharlak@wp.pl>
2022-11-27 21:47:30 +0100

    feat:Authentication, unique login in db
    When attempting to register, it is checked whether the user has entered the correct data and whether a user with the same login already exists. When trying to log in, it is checked whether the user exists, and then the data entered by the user is compared with the data from the database. If the data is true a JWT is returned.
     All possibilities are checked, such as not entering a password, entering bad data, etc., and corresponding responses with status codes are returned

--- src/main/java/api/ms/rest_api/Database.java
+++ src/main/java/api/ms/rest_api/Database.java
@@ -1,32 +1,137 @@
 package api.ms.rest_api;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.IndexOptions;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.RoutingContext;
 
-public class Database extends AbstractVerticle {
+import java.util.List;
+import java.util.UUID;
+
+public class Database {
 
   private final MongoClient client;
-  private final Vertx vertx;
+  private final Authorization authorization;
+  private final String collectionName = "users";
 
-  public Database(Vertx vertx) {
-    this.vertx = vertx;
-    JsonObject config = new JsonObject()
-      .put("db_name", "test")
-      .put("connection_string",
-        "mongodb://localhost:27017");
-    client = MongoClient.create(vertx, config);
+  public Database(Authorization authorization, Vertx vertx) {
+    this.authorization = authorization;
+    client = createDatabaseClient(vertx);
+    createIndexIfDatabaseIsNotCreated();
   }
 
-  public void saveUser(JsonObject jsonObject) {
-    client.save("users", jsonObject, res -> {
-      if (res.succeeded()) {
-        String id = res.result();
-        System.out.println("Saved user with id " + id);
-      } else {
-        res.cause().printStackTrace();
+  private MongoClient createDatabaseClient(Vertx vertx) {
+    String dbName = "test";
+    JsonObject config = new JsonObject()
+      .put("db_name", dbName)
+      .put("connection_string", "mongodb://localhost:27017");
+    return MongoClient.create(vertx, config);
+  }
+
+  private void createIndexIfDatabaseIsNotCreated() {
+    client.getCollections(listAsyncResult -> {
+      if (listAsyncResult.succeeded() && listAsyncResult.result().stream().noneMatch(s -> s.equals(collectionName))) {
+        client.createIndexWithOptions(collectionName, new JsonObject().put("login", 1), new IndexOptions().unique(true));
       }
     });
   }
+
+  public void saveUser(JsonObject jsonObject, RoutingContext context) {
+    JsonObject query = new JsonObject().put("login", jsonObject.getValue("login"));
+    client.find(collectionName, query, listAsyncResult -> {
+      if (listAsyncResult.succeeded() && listAsyncResult.result().isEmpty()) {
+        jsonObject.put("_id", UUID.randomUUID().toString());
+        client.save(collectionName, jsonObject);
+        this.responseWithTextAndCode("Registering successfull", 200, context);
+      } else this.responseWithTextAndCode("User with this login exists", 404, context);
+    });
+  }
+
+
+  public void getAllUsers(RoutingContext context) {
+    JsonObject entries = new JsonObject();
+    client.find(collectionName, entries, res -> {
+      if (res.succeeded()) {
+        List<JsonObject> jsonObjectList = res.result();
+        context.response().end(Json.encodePrettily(jsonObjectList));
+        for (JsonObject jsonObject : res.result()) {
+          System.out.println("jsonObject = " + jsonObject);
+        }
+      }
+    });
+  }
+
+  public void checkIfUsersIsInDatabase(JsonObject dataFromLogin, RoutingContext context) {
+    JsonObject query = new JsonObject().put("login", dataFromLogin.getValue("login"));
+
+//    Future<List<JsonObject>> listFuture = client.find(collectionName, query);
+//    System.out.println("listFuture = " + listFuture.succeeded());
+    // not workign ??????????????????????
+
+    client.find(collectionName, query, listAsyncResult -> {
+      if (listAsyncResult.succeeded() && !listAsyncResult.result().isEmpty()) {
+        JsonObject dataFromDb = listAsyncResult.result().get(0);
+        if (verifyUserIdentity(dataFromLogin, dataFromDb)) {
+          context
+            .response()
+            .setStatusCode(200)
+            .putHeader("content-type", "text/html; charset=UTF-8")
+            .end(authorization.createToken(dataFromDb.getString("login")));
+        } else {
+          context
+            .response()
+            .setStatusCode(401)
+            .putHeader("content-type", "text/html; charset=UTF-8")
+            .end("Login or password is incorrect");
+        }
+      } else {
+        context
+          .response()
+          .setStatusCode(401)
+          .putHeader("content-type", "text/html; charset=UTF-8")
+          .end("Login or password is incorrect");
+      }
+    });
+    //working ?????????? this is the SAMEEE
+
+  }
+
+  public boolean verifyUserIdentity(JsonObject dataFromLogin, JsonObject dataFromDb) {
+    return dataFromLogin.getString("password").equals(dataFromDb.getString("password"));
+  }
+
+  public void responseWithJsonAndCode(JsonObject jsonObject, int statusCode, RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(statusCode)
+      .putHeader("content-type", "application/json; charset=utf-8")
+      .end(Json.encodePrettily(jsonObject));
+  }
+
+  public void responseWithTextAndCode(String text, int statusCode, RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(statusCode)
+      .putHeader("content-type", "text/html; charset=UTF-8")
+      .end(text);
+  }
+
+  private void userNotRegisteredResponse(RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(404)
+      .setStatusMessage("Login or password invalid")
+      .end("please provide valid password and login");
+  }
+
+  private void userRegisteredResponse(RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(204)
+      .setStatusMessage("Registering successfull")
+      .end();
+  }
+
 }
