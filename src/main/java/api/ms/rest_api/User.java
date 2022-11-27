18bd70fffb7737abab891741dddbc04af6bc689b
Daaizo <danielcharlak@wp.pl>
2022-11-27 13:51:57 +0100

    feat: validation and appropriate response
    json data from the request body is checked for login key and password, if both present, login and password are validated.
    if the validation passes, the data is added to the db and a response is sent with status code 204 and the message "Registering successfull". If not, a response with status code 404 and the message "Login or password invalid" appears.

--- src/main/java/api/ms/rest_api/User.java
+++ src/main/java/api/ms/rest_api/User.java
@@ -8,32 +8,71 @@ import io.vertx.ext.web.RoutingContext;
 import java.util.UUID;
 
 public class User extends AbstractVerticle {
-  UUID id;
+  String id;
   String login;
   String password;
+  private Database database;
 
   public User() {
+    this.database = new Database(Vertx.vertx());
   }
 
   public User(String login, String password) {
-    this.id = UUID.randomUUID();
+    this.id = UUID.randomUUID().toString();
     this.login = login;
     this.password = password;
   }
 
-  public static void addUser(RoutingContext context) {
-    context.request().bodyHandler(buffer -> {
-      JsonObject object = buffer.toJsonObject();
-      System.out.println("object = " + object);
-      Database database = new Database(Vertx.vertx());
-      database.saveUser(object);
-    });
+  public User(JsonObject jsonObject) {
+    this.id = jsonObject.getString("_id");
+    this.login = jsonObject.getString("login");
+    this.password = jsonObject.getString("password");
+
+  }
+
+
+  public void addUser(RoutingContext context) {
+    context
+      .request()
+      .bodyHandler(buffer -> {
+        JsonObject object = buffer.toJsonObject();
+        if (!isJsonValid(object)) {
+          userNotRegisteredResponse(context);
+          return;
+        }
+        database.saveUser(object);
+        userRegisteredResponse(context);
+
+      });
+  }
+
+  private boolean isJsonValid(JsonObject jsonObject) {
+    if (jsonObject.isEmpty() || !jsonObject.containsKey("login") || !jsonObject.containsKey("password")) {
+      return false;
+    }
+    return Validate.isLoginValid(jsonObject.getString("login")) && Validate.isPasswordStrong(jsonObject.getString("password"));
+  }
+
+  private void userNotRegisteredResponse(RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(404)
+      .setStatusMessage("Login or password invalid")
+      .end("please provide valid password and login");
+
+  }
+
+  private void userRegisteredResponse(RoutingContext context) {
     context
       .response()
       .setStatusCode(204)
-      .putHeader("content-type", "application/json; charset=utf-8")
       .setStatusMessage("Registering successfull")
       .end();
+  }
+
+  public void getAllUsers(RoutingContext context) {
+    database.getAllUsers(context);
+
   }
 
   JsonObject getUserAsJson() {
