907d0785f7ac30376fc4f078eaa86bbf3f01f347
Daaizo <danielcharlak@wp.pl>
2022-11-29 08:16:14 +0100

    fix: saving item
    now item is saved to the new collection named items.
    code refactored

--- src/main/java/api/ms/rest_api/Database.java
+++ src/main/java/api/ms/rest_api/Database.java
@@ -14,7 +14,8 @@ public class Database {
 
   private final MongoClient client;
   private final Authorization authorization;
-  private final String collectionName = "users";
+  private final String collectionWithUsers = "users";
+  private final String collectionWithItems = "items";
 
   public Database(Authorization authorization, Vertx vertx) {
     this.authorization = authorization;
@@ -32,27 +33,27 @@ public class Database {
 
   private void createIndexIfDatabaseIsNotCreated() {
     client.getCollections(listAsyncResult -> {
-      if (listAsyncResult.succeeded() && listAsyncResult.result().stream().noneMatch(s -> s.equals(collectionName))) {
-        client.createIndexWithOptions(collectionName, new JsonObject().put("login", 1), new IndexOptions().unique(true));
+      if (listAsyncResult.succeeded() && listAsyncResult.result().stream().noneMatch(s -> s.equals(collectionWithUsers))) {
+        client.createIndexWithOptions(collectionWithUsers, new JsonObject().put("login", 1), new IndexOptions().unique(true));
       }
     });
   }
 
   public void saveUser(JsonObject jsonObject, RoutingContext context) {
     JsonObject query = new JsonObject().put("login", jsonObject.getValue("login"));
-    client.find(collectionName, query, listAsyncResult -> {
+
+    client.find(collectionWithUsers, query, listAsyncResult -> {
       if (listAsyncResult.succeeded() && listAsyncResult.result().isEmpty()) {
         jsonObject.put("_id", UUID.randomUUID().toString());
-        client.save(collectionName, jsonObject);
-        this.responseWithTextAndCode("Registering successfull", 204, context);
-      } else this.responseWithTextAndCode("User with this login exists", 409, context);
+        client.save(collectionWithUsers, jsonObject);
+        this.responseWithStatusDescriptionAndCode("Registering successfull", 204, context);
+      } else this.responseWithStatusDescriptionAndCode("User with this login exists", 409, context);
     });
   }
 
 
   public void getAllUsers(RoutingContext context) {
-    JsonObject entries = new JsonObject();
-    client.find(collectionName, entries, res -> {
+    client.find(collectionWithUsers, new JsonObject(), res -> {
       if (res.succeeded()) {
         List<JsonObject> jsonObjectList = res.result();
         this.responseWithTextAndCode(Json.encodePrettily(jsonObjectList), 200, context);
@@ -61,37 +62,32 @@ public class Database {
   }
 
   public void checkIfUsersIsInDatabase(JsonObject dataFromLogin, RoutingContext context) {
-    JsonObject query = new JsonObject().put("login", dataFromLogin.getValue("login"));
-    client.find(collectionName, query, listAsyncResult -> {
+    // JsonObject query = new JsonObject().put("login", dataFromLogin.getValue("login"));
+    client.find(collectionWithUsers, dataFromLogin, listAsyncResult -> {
       if (listAsyncResult.succeeded() && !listAsyncResult.result().isEmpty()) {
         JsonObject dataFromDb = listAsyncResult.result().get(0);
         if (verifyUserIdentity(dataFromLogin, dataFromDb)) {
           String token = authorization.createToken(dataFromDb.getString("login"));
-          authorization.checkToken(token);
           JsonObject jsonObject = new JsonObject().put("token", token);
           this.responseWithJsonAndCode(jsonObject, 200, context);
-        } else this.responseWithTextAndCode("Login or password is incorrect", 409, context);
-      } else this.responseWithTextAndCode("Login or password is incorrect", 409, context);
+        } else this.responseWithStatusDescriptionAndCode("Login or password is incorrect", 409, context);
+      } else this.responseWithStatusDescriptionAndCode("Login or password is incorrect", 409, context);
 
     });
   }
 
+  public void saveItem(JsonObject user, JsonObject itemFromRequestBody) {
+    String userId = user.getString("_id");
+    Item item = new Item(userId, itemFromRequestBody.getValue("title").toString());
+    client.save(collectionWithItems, item.getItemAsJson());
+  }
 
-  public void addItemForUser(JsonObject item, String userLogin, RoutingContext context) {
+  public void addItemForUser(JsonObject itemFromRequestBody, String userLogin, RoutingContext context) {
     JsonObject query = new JsonObject().put("login", userLogin);
-    client.find(collectionName, query, listAsyncResult -> {
+    client.find(collectionWithUsers, query, listAsyncResult -> {
       if (listAsyncResult.succeeded()) {
-        JsonObject user = listAsyncResult.result().get(0);
-        String userId = user.getString("_id");
-        System.out.println("userId = " + userId);
-        System.out.println("item " + item.encodePrettily());
-        JsonObject itemJson = new JsonObject(); // TODO wywoloac item z konstruktora klasy Item lub z metody jakiejs
-        itemJson
-          .put("_id", UUID.randomUUID().toString())
-          .put("owner", userId)
-          .put("name", item.getValue("title"));
-        client.save(collectionName, itemJson);
-        this.responseWithTextAndCode("Item created successfull", 204, context);
+        saveItem(listAsyncResult.result().get(0), itemFromRequestBody);
+        responseWithStatusDescriptionAndCode("Item created successfull", 204, context);
       }
     });
   }
@@ -115,6 +111,15 @@ public class Database {
       .setStatusCode(statusCode)
       .putHeader("content-type", "text/html; charset=UTF-8")
       .end(text);
+  }
+
+  public void responseWithStatusDescriptionAndCode(String text, int statusCode, RoutingContext context) {
+    context
+      .response()
+      .setStatusCode(statusCode)
+      .putHeader("content-type", "text/html; charset=UTF-8")
+      .setStatusMessage(text)
+      .end();
   }
 
 
--- src/main/java/api/ms/rest_api/Item.java
+++ src/main/java/api/ms/rest_api/Item.java
@@ -3,23 +3,45 @@ package api.ms.rest_api;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
+import java.util.UUID;
+
 public class Item {
-  private final Database database;
+  private UUID id;
+  private String ownerId;
+  private String name;
+  private Database database;
 
   public Item(Database database) {
     this.database = database;
   }
 
+  public Item(String ownerId, String name) {
+    this.id = UUID.randomUUID();
+    this.ownerId = ownerId;
+    this.name = name;
+  }
 
   public void addItem(RoutingContext context) {
     // String token = context.request().getHeader("Authorization");
     // String tokenValue = token.substring("Bearer".length());
-    String tokenOwnersLogin = context.user().principal().getString("name");
-    context
-      .request()
-      .bodyHandler(buffer -> {
-        JsonObject item = buffer.toJsonObject();
-        database.addItemForUser(item, tokenOwnersLogin, context);
-      });
+    try {
+      String tokenOwnersLogin = context.user().principal().getString("name");
+      context
+        .request()
+        .bodyHandler(buffer -> {
+          JsonObject item = buffer.toJsonObject();
+          database.addItemForUser(item, tokenOwnersLogin, context);
+        });
+    } catch (RuntimeException e) {
+      System.out.println("Unauthorized");
+    }
+  }
+
+  public JsonObject getItemAsJson() {
+    return
+      new JsonObject()
+        .put("_id", this.id.toString())
+        .put("owner", this.ownerId)
+        .put("name", this.name);
   }
 }
